333b2c9202874c50c537c043796152322e0a71db
爱吃菜的汪 <superfly1106@gmail.com>
2021-01-14 17:31:26 +0800

    上传jcenter

--- library/src/main/java/com/cai/bridge/Bridge.kt
+++
@@ -1,6 +0,0 @@
-package com.cai.bridge
-
-
-@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
-@kotlin.annotation.Retention(AnnotationRetention.BINARY)
-annotation class Bridge(val isMock: Boolean = false)
